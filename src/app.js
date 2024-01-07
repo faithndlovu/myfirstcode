97a3f59ab81097c2f7e552874e6d460bf1b3a1a6
Zanele Mpofu <148114843+faithndlovu@users.noreply.github.com>
2024-01-07 20:56:44 +0200

    adding basic search engine using js

---
+++ src/app.js
@@ -0,0 +1,8 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+let searchformElement = document.querySelector("#search-form");
+searchformElement.addEventListener("submit", handleSearchSubmit);
